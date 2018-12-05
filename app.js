d2ce275551f0eb00a266163a50cd6a57c5df4301
Rene Manzano <rene@Renes-MacBook.local>
2018-12-05 11:06:09 -0600

    heroku

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const bcrypt             = require('bcrypt');
 
 
 mongoose
-  .connect('process.env.DB', {useNewUrlParser: true})
+  .connect(process.env.DB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
